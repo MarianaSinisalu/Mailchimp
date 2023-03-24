71342f00a12b49a7dfe619e816558da9b881a057
MarianaSinisalu <marianasinisalu@gmail.com>
2023-03-24 11:34:21 +0100

    Mailchimp

--- src/test/java/myStepDefs/MyStepdefs.java
+++ src/test/java/myStepDefs/MyStepdefs.java
@@ -1,60 +1,135 @@
 package myStepDefs;
 import io.cucumber.java.After;
 import org.openqa.selenium.*;
-import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Date;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MyStepdefs {
     WebDriver driver;
+    private WebDriverWait wait;
 
-    @Before
-    public void setUp() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "/Users/mariana/Documents/Selenium/chromedriver_mac64/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
-        driver.get("https://login.mailchimp.com/signup/");
-        Thread.sleep(2000);
-    }
     @After
-    public void tearDown(){
-         driver.quit();
+    public void tearDown() {
+
+        driver.quit();
     }
-    @Given("I entered {string} address")
+
+    @Given("I use {string}")
+    public void iUse(String browser) {
+        if (browser.equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "/Users/mariana/Documents/Selenium/chromedriver_mac64/chromedriver");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--remote-allow-origins=*");
+            driver = new ChromeDriver(options);
+            driver.get("https://login.mailchimp.com/signup/");
+            driver.manage().window().maximize();
+        } else if (browser.equals("safari")) {
+
+            System.setProperty("webdriver.safari.driver", "/usr/bin/safaridriver");
+            driver = new SafariDriver();
+            driver.get("https://login.mailchimp.com/signup/");
+            driver.manage().window().maximize();
+        }
+    }
+
+    @When("I entered {string} address")
     public void iHaveEmailAddressString(String email) {
-        WebElement enterEmail = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[2]/main/div/div/form/fieldset/div[1]/div/input"));
-        enterEmail.sendKeys(email);
+        waitSendKeys(driver, By.id(("email")), email);
 
     }
-
-    @Given("I have {string}")
-    public void iHaveUsernameString(String username) {
-        WebElement enterUser = driver.findElement(By.id("new_username"));
+    private void waitSendKeys(WebDriver driver,By by, String text) {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(by));
+        element.sendKeys(text);
+    }
+    @When("I have {string}")
+    public void iHaveNewUsername(String username) {
+        WebElement enterUser = driver.findElement(By.cssSelector("#new_username"));
+        enterUser.click();
         enterUser.clear();
-        enterUser.sendKeys(username);
+        if (username.equals("newName")) {
+            Random random = new Random();
+            int randomNumber = random.nextInt(10000);
+            enterUser.sendKeys(randomNumber + "marianajunx" + randomNumber);
+
+        }
+
+        if (username.equals("long")) {
+
+            enterUser.sendKeys("randomStringrandomStringrandomStringrandomStringrandomStringrandomStringrandomStringrandomStringrandomStringrandomStringrandomString");
+        }
+
+        if (username.equals("taken")) {
+            enterUser.sendKeys("maria");
+        }
     }
 
-    @Given("I have entered {string}")
-    public void iHaveEnteredPasswordString(String password) throws InterruptedException {
+
+    @When("I have entered {string}")
+    public void iHaveEnteredPasswordString(String password) {
         WebElement enterPassword = driver.findElement(By.id("new_password"));
         enterPassword.sendKeys(password);
-        Thread.sleep(3000);
+        
+
+        
+
     }
 
     @When("I click Sign Up")
     public void iClickSignUp() {
         WebElement button = driver.findElement(By.id("create-account-enabled"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,500)", "");
         button.click();
+
     }
 
-    @Then("I can create an account")
-    public void iCanCreateAnAccount() {
+    @Then("I can {string} an account")
+    public void iCanAnAccount(String createAccount)   {
+
+         if (createAccount.equals("yes")) {
+             
+               if(driver.findElement(By.xpath("/html/body/div/div/div[2]")).isDisplayed()) {
+
+                       String actual = driver.getTitle();
+                       String expected = "Signup | Mailchimp";
+
+                       assertEquals(expected, actual);
+               } else {
+
+                   wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+                   wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[class='!margin-bottom--lv3 no-transform center-on-medium']")));
+
+                   String actual = driver.findElement(By.cssSelector("[class='!margin-bottom--lv3 no-transform center-on-medium'")).getText();
+                   String expected = "Check your email";
+
+
+                   assertEquals(expected, actual);
+               }
+        }else if(createAccount.equalsIgnoreCase("no")){
+            String actual = driver.getTitle();
+            String expected = "Signup | Mailchimp";
+
+            assertEquals(expected, actual);
+        }
+
+
 
     }
 }
+
+
+
--- src/test/resources/features/Mailchimp.feature
+++ src/test/resources/features/Mailchimp.feature
@@ -1,13 +1,17 @@
 Feature: Mailchimp
 
   Scenario Outline: Sign up
-    Given I entered "<email>" address
-    Given I have "<username>"
-    Given I have entered "<password>"
+    Given I use "<browser>"
+    When I entered "<email>" address
+    When I have "<username>"
+    When I have entered "<password>"
     When I click Sign Up
-    Then I can create an account
+    Then I can "<create>" an account
 
 
   Examples:
-    | email                |   username        | password        |
-    |marianasini@gmail.com |   marianajunx     | marianaJunx5!   |
+    | browser | email                    | username | password      | create |
+    | chrome  | arianasinisalu@gmail.com | newName  | marianaJunx5! | yes    |
+    | safari  | arianasinisalu@gmail.com | long     | marianaJunx5! | no     |
+    | safari  | arianasinisalu@gmail.com | taken    | marianaJunx5! | no     |
+    | chrome  |                          | newName  | marianaJunx5! | no     |
